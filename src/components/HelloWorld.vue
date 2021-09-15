1400c3124c18c80047c190afa59222819fd1b7a7
SongZhou <810379677@qq.com>
2021-09-15 14:35:43 +0800

    style: update style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="hello">
+  <!-- <div class="hello">
     <a-button @click="onOpenModal">我是第一个按钮</a-button>
+  </div> -->
+  <div id="components-layout-demo-basic" style="height: 100%">
+    <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout>
+        <a-layout-header>Header</a-layout-header>
+        <a-layout-content>Content</a-layout-content>
+        <a-layout-footer>Footer</a-layout-footer>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
@@ -42,4 +52,44 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#components-layout-demo-basic .code-box-demo {
+  text-align: center;
+}
+#components-layout-demo-basic .ant-layout.ant-layout-has-sider {
+  height: 100%;
+}
+#components-layout-demo-basic .ant-layout-header,
+#components-layout-demo-basic .ant-layout-footer {
+  color: #fff;
+  background: #7dbcea;
+}
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-header {
+  background: #6aa0c7;
+}
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-footer {
+  background: #6aa0c7;
+}
+#components-layout-demo-basic .ant-layout-footer {
+  line-height: 1.5;
+}
+#components-layout-demo-basic .ant-layout-sider {
+  color: #fff;
+  line-height: 120px;
+  background: #3ba0e9;
+}
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-sider {
+  background: #3499ec;
+}
+#components-layout-demo-basic .ant-layout-content {
+  min-height: 120px;
+  color: #fff;
+  line-height: 120px;
+  background: rgba(16, 142, 233, 1);
+}
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-content {
+  background: #107bcb;
+}
+#components-layout-demo-basic > .code-box-demo > .ant-layout + .ant-layout {
+  margin-top: 48px;
+}
 </style>
